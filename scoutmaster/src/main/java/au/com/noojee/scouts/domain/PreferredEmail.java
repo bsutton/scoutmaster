edd29d6e2edfae7686c9cc5797d10a534e1b1043
bsutton@noojee.com.au <bsutton@noojee.com.au>
2013-07-07 22:55:55 +1000

    Partial implementation of login/forgotten password forms. Partial implementation of Menubar generated from annotated Views. Now injecting the EntityManager it each request thread. Rework of unit tests to use separate test db and work with the new style EntityManager. Nothing is really working but things are starting to form up.

--- scoutmaster/src/main/java/au/com/noojee/scouts/domain/PreferredEmail.java
+++
@@ -1,6 +0,0 @@
-package au.com.noojee.scouts.domain;
-
-public enum PreferredEmail
-{
-	HOME, WORK;
-}
--- scoutmaster/src/main/java/au/com/noojee/scouts/domain/TryoutOutcome.java
+++
@@ -1,6 +0,0 @@
-package au.com.noojee.scouts.domain;
-
-public enum TryoutOutcome
-{
-	NOT_INTERESTED, JOINED, MAY_JOIN_LATER
-}
