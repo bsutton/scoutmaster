b00ccb05cdd06cf8a6d1c2f042e4be8522fd69a6
bsutton@noojee.com.au <bsutton@noojee.com.au>
2013-07-21 22:31:54 +1000

    A working version of the Contact CRUD interface sans a couple of key fields - Section, Tag and Address.

--- scoutmaster/src/main/java/au/org/scoutmaster/domain/Period.java
+++ scoutmaster/src/main/java/au/org/scoutmaster/domain/Period.java
@@ -3,6 +3,8 @@ package au.org.scoutmaster.domain;
 import javax.persistence.Basic;
 import javax.persistence.Embeddable;
 
+import org.joda.time.Interval;
+
 @Embeddable
 public class Period
 {
@@ -11,18 +13,26 @@ public class Period
 	@Basic
 	private int months;
 	@Basic
-	private  int days;
+	private int days;
 
 	public Period()
 	{
-		
+
 	}
+
 	public Period(int years, int months, int days)
 	{
 		this.years = years;
 		this.months = months;
 		this.days = days;
 
+	}
+
+	public Period(Interval interval)
+	{
+		this.years = interval.toPeriod().getYears();
+		this.months = interval.toPeriod().getMonths();
+		this.days = interval.toPeriod().getDays();
 	}
 
 	public int getYears()
@@ -40,6 +50,32 @@ public class Period
 		return days;
 	}
 
+	public String toString()
+	{
+		StringBuilder sb = new StringBuilder();
+
+		boolean prior = false;
+		if (years > 0)
+		{
+			sb.append(years + "y");
+			prior = true;
+		}
+
+		if (months > 0)
+		{
+			if (prior)
+				sb.append(", ");
+			sb.append(months + "m");
+			prior = true;
+		}
+		if (days > 0)
+		{
+			if (prior)
+				sb.append(", ");
+			sb.append(days + "d");
+		}
+		return sb.toString();
+	}
 }
 
 class Builder
@@ -52,23 +88,23 @@ class Builder
 	{
 		return new Period(this.years, this.months, this.days);
 	}
-	
+
 	Builder setYears(int years)
 	{
 		this.years = years;
 		return this;
 	}
-	
+
 	Builder setMonths(int months)
 	{
 		this.months = months;
 		return this;
 	}
-	
+
 	Builder setDays(int days)
 	{
 		this.days = days;
 		return this;
 	}
-	
+
 }
