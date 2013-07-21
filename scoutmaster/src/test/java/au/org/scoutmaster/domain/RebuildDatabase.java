7d049055050e861b945b4097ede67b68bb3f789d
bsutton@noojee.com.au <bsutton@noojee.com.au>
2013-07-21 22:44:19 +1000

    really a tool for dumping the db.

---
+++ scoutmaster/src/test/java/au/org/scoutmaster/domain/RebuildDatabase.java
@@ -0,0 +1,44 @@
+package au.org.scoutmaster.domain;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.ServletException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import au.org.scoutmaster.filter.EntityManagerProvider;
+
+/**
+ * Use this unit test to rebuild the database from the JPA domain entities.
+ * @author bsutton
+ *
+ */
+public class RebuildDatabase
+{
+
+	private EntityManagerFactory entityManagerFactory;
+	private EntityManager em;
+
+	@Before
+	public void init() throws ServletException
+	{
+		entityManagerFactory = Persistence.createEntityManagerFactory("scoutmastertest");
+		em = entityManagerFactory.createEntityManager();
+		EntityManagerProvider.INSTANCE.setCurrentEntityManager(em);
+	}
+
+	@Test
+	public void test()
+	{
+		System.out.println("Database has been rebuilt. Run: (password = master$4scout) \n");
+		System.out.println("Before you run this script you should have first dropped and recreated the scoutmastertest database");
+		System.out.println("mysqldump --triggers --routines -u scoutmaster -p scoutmastertest > init.sql\n");
+		System.out.println("Copy init.sql into /src/main/resources/liqibase \n");
+		System.out.println("Edit the init.sql and remove the comments from the SET commands at the start and end of the script.");
+		System.out.println("Drop and create the scoutmaster database.");
+		System.out.println("Start Scoutmaster and your db should be recreated");
+	}
+
+}
