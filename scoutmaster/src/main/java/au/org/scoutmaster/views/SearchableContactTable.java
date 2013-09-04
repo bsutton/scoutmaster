ba99cc90153d392f1099b35765ff114c2b305f78
bsutton@noojee.com.au <bsutton@noojee.com.au>
2013-09-04 23:49:18 +1000

    almost have the search filter working.

--- scoutmaster/src/main/java/au/org/scoutmaster/views/SearchableContactTable.java
+++ scoutmaster/src/main/java/au/org/scoutmaster/views/SearchableContactTable.java
@@ -3,21 +3,23 @@ package au.org.scoutmaster.views;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Join;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
+import javax.persistence.metamodel.SingularAttribute;
 
 import org.apache.log4j.Logger;
 
+import au.com.vaadinutils.crud.ColumnList;
+import au.com.vaadinutils.crud.EntityTable;
+import au.com.vaadinutils.crud.HeadingToPropertyId;
+import au.com.vaadinutils.crud.RowChangeListener;
+import au.org.scoutmaster.dao.filter.TagFilter;
 import au.org.scoutmaster.domain.Contact;
+import au.org.scoutmaster.domain.Contact_;
 import au.org.scoutmaster.domain.Tag;
 import au.org.scoutmaster.fields.TagChangeListener;
 import au.org.scoutmaster.fields.TagField;
 
 import com.vaadin.addon.jpacontainer.JPAContainer;
-import com.vaadin.addon.jpacontainer.util.DefaultQueryModifierDelegate;
+import com.vaadin.data.Container.Filter;
 import com.vaadin.data.util.filter.Or;
 import com.vaadin.data.util.filter.SimpleStringFilter;
 import com.vaadin.event.FieldEvents.TextChangeEvent;
@@ -30,19 +32,20 @@ import com.vaadin.ui.VerticalLayout;
 
 public class SearchableContactTable extends VerticalLayout implements TagChangeListener
 {
+	@SuppressWarnings("unused")
 	private static Logger logger = Logger.getLogger(SearchableContactTable.class);
 	private static final long serialVersionUID = 1L;
-	private ContactTable contactTable;
+	private EntityTable<Contact> contactTable;
 	private TextField searchField = new TextField();
 	private VerticalLayout searchLayout = new VerticalLayout();
 	private JPAContainer<Contact> contactContainer;
 	private HorizontalLayout stringSearchLayout;
 	private Button clearButton = new Button("Clear");
 
-	public SearchableContactTable(JPAContainer<Contact> contactContainer, String[] visibleColumns)
+	public SearchableContactTable(JPAContainer<Contact> contactContainer, List<HeadingToPropertyId> visibleColumns)
 	{
 		this.contactContainer = contactContainer;
-		this.contactTable = new ContactTable(contactContainer, visibleColumns);
+		this.contactTable = new EntityTable<Contact>(contactContainer, visibleColumns);
 		init();
 	}
 
@@ -71,7 +74,7 @@ public class SearchableContactTable extends VerticalLayout implements TagChangeL
 	{
 		searchLayout = new VerticalLayout();
 		HorizontalLayout tagSearchLayout = new HorizontalLayout();
-		TagField tagField = new TagField("Search Tags", true);
+		final TagField tagField = new TagField("Search Tags", true);
 		tagSearchLayout.addComponent(tagField);
 
 		tagField.addChangeListener(this);
@@ -117,76 +120,150 @@ public class SearchableContactTable extends VerticalLayout implements TagChangeL
 			{
 
 				/* Reset the filter for the contactContainer. */
-				contactContainer.removeAllContainerFilters();
-				contactContainer.addContainerFilter(new Or(new SimpleStringFilter(Contact.FIRSTNAME, event.getText(),
-						true, false), new SimpleStringFilter(Contact.LASTNAME, event.getText(), true, false)));
+				resetQueryBuilder(tagField.getTags(), event.getText());
+//				contactContainer.removeAllContainerFilters();
+//				contactContainer.addContainerFilter(new Or(new SimpleStringFilter(Contact.FIRSTNAME, event.getText(),
+//						true, false), new SimpleStringFilter(Contact.LASTNAME, event.getText(), true, false)));
 
 			}
 		});
 	}
 
+	
 	public void onTagListChanged(ArrayList<Tag> tags)
 	{
+		resetQueryBuilder(tags, this.searchField.getValue());
+
+	}
+
+	void resetQueryBuilder(ArrayList<Tag> tags, String fullTextSearch)
+	{
 		contactContainer.removeAllContainerFilters();
-		buildQuery(tags);
+		contactContainer.getEntityProvider().setQueryModifierDelegate(new ContactDefaultQueryModifierDelegate(tags, fullTextSearch));
 		contactTable.refreshRowCache();
 
-		// if (tags.size() > 0)
-		// {
-		// Filter[] filters = new Filter[tags.size()];
-		//
-		// int i = 0;
-		// for (Tag tag : tags)
-		// {
-		// filters[i++] = new Equal("tags.id", tag.getId());
-		// }
-		//
-		// contactContainer.addContainerFilter(new Or(filters));
-		// }
-
 	}
+//
+//	public void onTagListChanged(ArrayList<Tag> tags)
+//	{
+//		contactContainer.removeAllContainerFilters();
+//		buildQuery(tags);
+//		contactTable.refreshRowCache();
+//
+//		// if (tags.size() > 0)
+//		// {
+//		// Filter[] filters = new Filter[tags.size()];
+//		//
+//		// int i = 0;
+//		// for (Tag tag : tags)
+//		// {
+//		// filters[i++] = new Equal("tags.id", tag.getId());
+//		// }
+//		//
+//		// contactContainer.addContainerFilter(new Or(filters));
+//		// }
+//
+//	}
+//	public void onTagListChanged(ArrayList<Tag> tags)
+//	{
+//		contactContainer.removeAllContainerFilters();
+//		contactContainer.addContainerFilter(getContainerFilter(tags));
+//		contactTable.refreshRowCache();
+//	}
 
-	void buildQuery(final ArrayList<Tag> tags)
+	protected Filter getContainerFilter(ArrayList<Tag> tags)
 	{
-		contactContainer.getEntityProvider().setQueryModifierDelegate(new DefaultQueryModifierDelegate()
+		Filter filter = null;
+
+		if (tags.size() > 0)
 		{
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public void filtersWillBeAdded(CriteriaBuilder cb, CriteriaQuery<?> query, List<Predicate> predicates)
+			for (Tag tag : tags)
 			{
-				@SuppressWarnings("unchecked")
-				Root<Contact> fromContact = (Root<Contact>) query.getRoots().iterator().next();
-
-				if (tags.size() > 0)
+				if (filter == null)
 				{
-					try
-					{
-						Tag firstTag = tags.get(0);
-						Join<Contact, Tag> tagJoin = fromContact.join("tags");
-						predicates.add(cb.equal(tagJoin.get("id"), firstTag.getId()));
-					}
-					catch (Throwable e)
-					{
-						logger.error(e, e);
-					}
+					filter = new TagFilter(tag);
 				}
+				else
+					filter = new Or(new TagFilter(tag), filter);
+			}
+
+			ColumnList list = new ColumnList(Contact_.firstname, Contact_.lastname, Contact_.birthDate);
+
+			String searchString = this.searchField.getValue();
+			for (SingularAttribute<? extends Object, ? extends Object> column : list.getList())
+			{
+				if (filter == null)
+				{
+					filter = new SimpleStringFilter(column.getName(), searchString, true, false);
+				}
+				filter = new Or(new SimpleStringFilter(column.getName(), searchString, true, false), filter);
 
 			}
-		});
+		}
 
+		return filter;
 	}
 
+	// try
+	// {
+	// if (tags.size() > 0)
+	// {
+	// Subquery<Contact> subquery = query.subquery(Contact.class);
+	// Root<Contact> sqRoot = subquery.from(Contact.class);
+	// sqRoot.alias("C");
+	// Join<Contact, Tag> tagJoin = sqRoot.join(Contact_.tags);
+	//
+	// List<Predicate> tagPredicates = new ArrayList<>();
+	// for (Tag tag : tags)
+	// {
+	//
+	// Predicate tagPredicate = builder.equal(tagJoin.get(Tag_.id),
+	// tag.getId());
+	// tagPredicates.add(tagPredicate);
+	// }
+	// Predicate[] orPredicates = new Predicate[tagPredicates.size()];
+	// Predicate or = builder.or((Predicate[])
+	// tagPredicates.toArray(orPredicates));
+	//
+	// ParameterExpression<Long> longParameter = builder.parameter(Long.class);
+	// Predicate typePredicate = builder.equal(sqRoot.get(Contact_.id),
+	// longParameter);
+	//
+	// // i have not tried this before but I assume this will correlate the
+	// subquery with the parent root entity
+	// Predicate correlatePredicate = builder.equal(sqRoot.get(Contact_.id),
+	// fromContact);
+	// Predicate parentCorrelation = builder.and(typePredicate,
+	// correlatePredicate);
+	// subquery.where(builder.and(parentCorrelation, or));
+	// query.where(builder.exists(subquery));
+	// query.distinct(true);
+	// }
+	// }
+	// catch (Throwable e)
+	// {
+	// logger.error(e, e);
+	// }
+
+	//
+	// void resetQueryBuilder(ArrayList<Tag> tags)
+	// {
+	// contactContainer.getEntityProvider().setQueryModifierDelegate(new
+	// ContactDefaultQueryModifierDelegate(tags));
+	// contactContainer.removeAllContainerFilters();
+	//
+	// }
+	//
 	public int size()
 	{
 		return contactTable.size();
 	}
-	
+
 	public ArrayList<Contact> getFilteredContacts()
 	{
 		ArrayList<Contact> contacts = new ArrayList<>();
-		
-		for (Object itemId :contactContainer.getItemIds())
+
+		for (Object itemId : contactContainer.getItemIds())
 		{
 			Contact contact = contactContainer.getItem(itemId).getEntity();
 			contacts.add(contact);
