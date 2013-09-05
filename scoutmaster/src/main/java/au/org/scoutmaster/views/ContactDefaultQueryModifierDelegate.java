77e36badd131968c3df7babeb2eb71c91318a62c
bsutton@noojee.com.au <bsutton@noojee.com.au>
2013-09-05 23:12:18 +1000

    Actually now have a fully working query!

--- scoutmaster/src/main/java/au/org/scoutmaster/views/ContactDefaultQueryModifierDelegate.java
+++ scoutmaster/src/main/java/au/org/scoutmaster/views/ContactDefaultQueryModifierDelegate.java
@@ -6,22 +6,19 @@ import java.util.List;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Join;
-import javax.persistence.criteria.Order;
-import javax.persistence.criteria.ParameterExpression;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import javax.persistence.criteria.Subquery;
 
 import org.apache.log4j.Logger;
 
+import au.com.vaadinutils.filters.QueryModifierAdaptor;
 import au.org.scoutmaster.domain.Contact;
 import au.org.scoutmaster.domain.Contact_;
+import au.org.scoutmaster.domain.SectionType_;
 import au.org.scoutmaster.domain.Tag;
-import au.org.scoutmaster.domain.Tag_;
 
-import com.vaadin.addon.jpacontainer.QueryModifierDelegate;
-
-public class ContactDefaultQueryModifierDelegate implements QueryModifierDelegate
+public class ContactDefaultQueryModifierDelegate extends  QueryModifierAdaptor
 {
 	private static final long serialVersionUID = 1L;
 	private static Logger logger = Logger.getLogger(ContactDefaultQueryModifierDelegate.class);
@@ -33,20 +30,6 @@ public class ContactDefaultQueryModifierDelegate implements QueryModifierDelegat
 	{
 		this.tags = tags;
 		this.fullTextSearch = fullTextSearch;
-
-	}
-
-	@Override
-	public void queryWillBeBuilt(CriteriaBuilder criteriaBuilder, CriteriaQuery<?> query)
-	{
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void queryHasBeenBuilt(CriteriaBuilder criteriaBuilder, CriteriaQuery<?> query)
-	{
-
 	}
 
 	@Override
@@ -57,9 +40,10 @@ public class ContactDefaultQueryModifierDelegate implements QueryModifierDelegat
 
 		try
 		{
+			Subquery<Contact> subquery = null;
 			if (tags.size() > 0)
 			{
-				Subquery<Contact> subquery = query.subquery(Contact.class);
+				subquery = query.subquery(Contact.class);
 				Root<Contact> subqueryContact = subquery.from(Contact.class);
 				subqueryContact.alias("C");
 				Join<Contact, Tag> tagJoin = subqueryContact.join(Contact_.tags);
@@ -79,82 +63,40 @@ public class ContactDefaultQueryModifierDelegate implements QueryModifierDelegat
 				Predicate subqueryPredicate = builder.equal(subqueryContact.get("id"), fromContact.get("id"));
 				subquery.where(builder.and(subqueryPredicate, or));
 
-				// Build main queries full text search
-				if (fullTextSearch != null && !fullTextSearch.isEmpty())
-				{
-					Predicate fullTextSearchPredicate = builder.like(
-							builder.upper(fromContact.get(Contact_.firstname)), "%" + this.fullTextSearch.toUpperCase()
-									+ "%");
-					fullTextSearchPredicate = builder.or(builder.like(
-							builder.upper(fromContact.get(Contact_.lastname)), "%" + this.fullTextSearch.toUpperCase()
-									+ "%"), fullTextSearchPredicate);
-
-					query.where(builder.and(fullTextSearchPredicate, builder.exists(subquery)));
-				}
-				else
-					query.where(builder.exists(subquery));
-				// query.distinct(true);
 			}
 
-		}
-		catch (Throwable e)
-		{
-			logger.error(e, e);
-		}
-
-	}
-
-	public void originalfiltersWillBeAdded(CriteriaBuilder builder, CriteriaQuery<?> query, List<Predicate> predicates)
-	{
-		@SuppressWarnings("unchecked")
-		Root<Contact> fromContact = (Root<Contact>) query.getRoots().iterator().next();
-
-		try
-		{
-			if (tags.size() > 0)
+			Predicate fullTextSearchPredicate = null;
+			// Build main queries full text search
+			if (fullTextSearch != null && !fullTextSearch.isEmpty())
 			{
-				Join<Contact, Tag> tagJoin = fromContact.join("tags");
+				fullTextSearchPredicate = builder.like(builder.upper(fromContact.get(Contact_.firstname)), "%"
+						+ this.fullTextSearch.toUpperCase() + "%");
+				fullTextSearchPredicate = builder.or(
+						builder.like(builder.upper(fromContact.get(Contact_.lastname)),
+								"%" + this.fullTextSearch.toUpperCase() + "%"), fullTextSearchPredicate);
+				
+				fullTextSearchPredicate = builder.or(
+						builder.like(builder.function("date_format", String.class, fromContact.get(Contact_.birthDate), builder.literal("%Y-%m-%d")),
+								"%" + this.fullTextSearch.toUpperCase() + "%"), fullTextSearchPredicate);
 
-				List<Predicate> tagPredicates = new ArrayList<>();
-				for (Tag tag : tags)
-				{
-
-					Predicate tagPredicate = builder.equal(tagJoin.get("id"), tag.getId());
-					tagPredicates.add(tagPredicate);
-				}
-				Predicate[] orPredicates = new Predicate[tagPredicates.size()];
-				Predicate or = builder.or((Predicate[]) tagPredicates.toArray(orPredicates));
-				predicates.add(or);
-				query.distinct(true);
-
+				fullTextSearchPredicate = builder.or(
+						builder.like(builder.upper(fromContact.get(Contact_.section).get(SectionType_.name)),
+								"%" + this.fullTextSearch.toUpperCase() + "%"), fullTextSearchPredicate);
+				
 			}
+
+			if (subquery != null && fullTextSearchPredicate != null)
+				query.where(builder.and(fullTextSearchPredicate, builder.exists(subquery)));
+			else if (subquery != null)
+				query.where(builder.exists(subquery));
+			else if (fullTextSearchPredicate != null)
+				query.where(fullTextSearchPredicate);
+
 		}
 		catch (Throwable e)
 		{
 			logger.error(e, e);
 		}
-
-	}
-
-	@Override
-	public void filtersWereAdded(CriteriaBuilder criteriaBuilder, CriteriaQuery<?> query)
-	{
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void orderByWillBeAdded(CriteriaBuilder criteriaBuilder, CriteriaQuery<?> query, List<Order> orderBy)
-	{
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void orderByWasAdded(CriteriaBuilder criteriaBuilder, CriteriaQuery<?> query)
-	{
-		// TODO Auto-generated method stub
-
 	}
 
 }
