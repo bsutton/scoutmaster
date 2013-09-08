21e882cf22be30ce0a6c21de64439e28827774c4
bsutton@noojee.com.au <bsutton@noojee.com.au>
2013-09-09 09:19:56 +1000

    New class to show errors during an import.

--- scoutmaster/src/main/java/au/org/scoutmaster/views/wizards/importer/ImportItemStatus.java
+++ scoutmaster/src/main/java/au/org/scoutmaster/views/wizards/importer/ImportItemStatus.java
@@ -1,6 +1,33 @@
 package au.org.scoutmaster.views.wizards.importer;
 
+import javax.validation.ConstraintViolationException;
+
 public class ImportItemStatus
 {
+	public Integer getRow()
+	{
+		return row;
+	}
+
+	public Exception getException()
+	{
+		return exception;
+	}
+
+	Integer row;
+	Exception exception;
+
+	public ImportItemStatus(Integer row, Exception e)
+	{
+		this.row = row;
+		this.exception = new ImportRowExeption(e);
+	}
+
+	public ImportItemStatus(int row, ConstraintViolationException e)
+	{
+		this.row = row;
+		this.exception = new ImportRowExeption(e);
+
+	}
 
 }
