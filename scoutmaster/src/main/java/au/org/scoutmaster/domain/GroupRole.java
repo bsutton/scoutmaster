0b9837aec49dfa2050a004c764518f2b103dbd89
bsutton@noojee.com.au <bsutton@noojee.com.au>
2013-09-10 05:59:23 +1000

    Added Recruitment Officer.

--- scoutmaster/src/main/java/au/org/scoutmaster/domain/GroupRole.java
+++ scoutmaster/src/main/java/au/org/scoutmaster/domain/GroupRole.java
@@ -2,6 +2,6 @@ package au.org.scoutmaster.domain;
 
 public enum GroupRole
 {
-	YouthMember, Parent, Gardian, AdultHelper, Volunteer, Leader, AssistantLeader, President, Secretary, Treasurer, QuarterMaster, GroupLeader, CommitteeMember
+	YouthMember, Parent, Gardian, AdultHelper, Volunteer, Leader, AssistantLeader, President, Secretary, Treasurer, QuarterMaster, GroupLeader, CommitteeMember, RecruitmentOfficer
 
 }
