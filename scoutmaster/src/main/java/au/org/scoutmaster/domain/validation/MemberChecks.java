6f8633f321e48fda22e64cabad4e286a517e96b9
bsutton@noojee.com.au <bsutton@noojee.com.au>
2013-09-10 05:59:52 +1000

    validation interface for the Membership validation group.

---
+++ scoutmaster/src/main/java/au/org/scoutmaster/domain/validation/MemberChecks.java
@@ -0,0 +1,13 @@
+package au.org.scoutmaster.domain.validation;
+
+/**
+ * Defines as validation group for Members on the Contact.
+ * 
+ * This group should only be validated if the Contact is a member.
+ * @author bsutton
+ *
+ */
+public interface MemberChecks
+{
+
+}
