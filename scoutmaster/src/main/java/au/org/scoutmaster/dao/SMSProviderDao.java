7d3c327697849c8cd67d4beada9f7bd9dd1e58eb
bsutton@noojee.com.au <bsutton@noojee.com.au>
2013-09-11 23:02:45 +1000

    Refactored to use SMSSession.

--- scoutmaster/src/main/java/au/org/scoutmaster/dao/SMSProviderDao.java
+++ scoutmaster/src/main/java/au/org/scoutmaster/dao/SMSProviderDao.java
@@ -1,30 +1,22 @@
 package au.org.scoutmaster.dao;
 
 import java.io.IOException;
-import java.sql.Date;
 import java.util.List;
 
 import org.apache.log4j.Logger;
-import org.joda.time.DateTime;
-import org.marre.SmsSender;
 import org.marre.sms.SmsException;
 
-import au.org.scoutmaster.domain.Activity;
-import au.org.scoutmaster.domain.ActivityType;
-import au.org.scoutmaster.domain.Phone;
 import au.org.scoutmaster.domain.SMSProvider;
-import au.org.scoutmaster.domain.access.User;
 import au.org.scoutmaster.util.ProgressListener;
+import au.org.scoutmaster.util.SMSSession;
 import au.org.scoutmaster.views.wizards.messaging.Message;
 import au.org.scoutmaster.views.wizards.messaging.SMSTransmission;
 
 import com.vaadin.addon.jpacontainer.JPAContainer;
-import com.vaadin.ui.UI;
 
 public class SMSProviderDao extends JpaBaseDao<SMSProvider, Long> implements Dao<SMSProvider, Long>
 {
 	Logger logger = Logger.getLogger(SMSProviderDao.class);
-	private SmsSender smsSender;
 
 	@Override
 	public List<SMSProvider> findAll()
@@ -37,77 +29,78 @@ public class SMSProviderDao extends JpaBaseDao<SMSProvider, Long> implements Dao
 		return super.findListBySingleParameter(SMSProvider.FIND_BY_NAME, "name", name);
 	}
 
+	/**
+	 * Sends a list of SMS messages using a single provider session.
+	 * 
+	 * @param provider
+	 * @param targets
+	 * @param message
+	 * @param listener
+	 * @throws SmsException
+	 * @throws IOException
+	 */
 	public void send(SMSProvider provider, List<SMSTransmission> targets, Message message,
-			ProgressListener<SMSTransmission> listener) throws SmsException 
+			ProgressListener<SMSTransmission> listener) throws SmsException, IOException
 	{
-
-		init(provider);
-
 		int max = targets.size();
 		int count = 0;
-		for (SMSTransmission transmission : targets)
-		{
-			try
-			{
-				count++;
-				listener.progress(count, max, transmission);
-				send(transmission);
-			}
-			catch (SmsException | IOException e)
-			{
-				transmission.setException(e);
-				listener.itemError(e, transmission);
-				logger.error(e, e);
-			}
 
+		try (SMSSession session = new SMSSession(provider))
+		{
+			for (SMSTransmission transmission : targets)
+			{
+				try
+				{
+					count++;
+					listener.progress(count, max, transmission);
+					session.send(transmission);
+				}
+				catch (SmsException | IOException e)
+				{
+					transmission.setException(e);
+					listener.itemError(e, transmission);
+					logger.error(e, e);
+				}
+
+			}
 		}
+		catch (IOException e)
+		{
+			logger.error(e, e);
+			throw e;
+		}
+
 		listener.complete();
 	}
 
+	/**
+	 * Sends a single SMS messaging using its own session.
+	 * @param provider
+	 * @param transmission
+	 * @param listener
+	 * @throws SmsException
+	 * @throws IOException
+	 */
 	public void send(SMSProvider provider, SMSTransmission transmission, ProgressListener<SMSTransmission> listener)
 			throws SmsException, IOException
 	{
 
-		init(provider);
+		try (SMSSession session = new SMSSession(provider))
+		{
 
-		send(transmission);
-		listener.progress(1, 1, transmission);
-		listener.complete();
-	}
-
-	private void send(SMSTransmission transmission) throws SmsException, IOException
-	{
-		// The message that you want to send.
-		String msg = transmission.getMessage().getSubject();
-
-		// International number to target without leading "+"
-		Phone reciever = transmission.getPhone();
-
-		// Number of sender (not supported on all transports)
-		smsSender.connect();
-		smsSender.sendTextSms(msg, reciever.getPhoneNo(), transmission.getMessage().getSender().getPhoneNo());
-		smsSender.disconnect();
-
-		// Log the activity
-		ActivityDao daoActivity = new DaoFactory().getActivityDao();
-		ActivityTypeDao daoActivityType = new DaoFactory().getActivityTypeDao();
-		Activity activity = new Activity();
-		User user = (User) UI.getCurrent().getSession().getAttribute("user");
-		activity.setAddedBy(user);
-		activity.setSubject("SMSMessage sent");
-		activity.setType(daoActivityType.findByName(ActivityType.BULK_SMS));
-		activity.setActivityDate((Date) new DateTime().toDate());
-		activity.setDetails("Subject: " + transmission.getMessage().getSubject() + "Phone: " + reciever + " Message:"
-				+ transmission.getMessage().getBody());
-		daoActivity.persist(activity);
-	}
-
-	private void init(SMSProvider provider) throws SmsException
-	{
-		smsSender = SmsSender.getClickatellSender(provider.getUsername(), provider.getPassword(), provider.getApiId());
+			session.send(transmission);
+			listener.progress(1, 1, transmission);
+			listener.complete();
+		}
+		catch (IOException e)
+		{
+			logger.error(e, e);
+			throw e;
+		}
 
 	}
 
+	
 	@Override
 	public JPAContainer<SMSProvider> makeJPAContainer()
 	{
