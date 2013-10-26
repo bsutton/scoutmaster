57be133ce5e9d4149feadf791b59565bf79ea243
bsutton@noojee.com.au <bsutton@noojee.com.au>
2013-10-27 07:59:40 +1100

    regenerated.

--- scoutmaster/src/main/java/au/org/scoutmaster/domain/access/User_.java
+++ scoutmaster/src/main/java/au/org/scoutmaster/domain/access/User_.java
@@ -6,16 +6,16 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2013-10-22T22:20:10.043+1100")
+@Generated(value="Dali", date="2013-10-23T14:21:31.707+1100")
 @StaticMetamodel(User.class)
 public class User_ extends BaseEntity_ {
 	public static volatile SingularAttribute<User, String> username;
 	public static volatile SingularAttribute<User, String> saltedPassword;
+	public static volatile SingularAttribute<User, String> firstname;
+	public static volatile SingularAttribute<User, String> surname;
 	public static volatile SingularAttribute<User, String> emailAddress;
 	public static volatile SingularAttribute<User, Boolean> enabled;
 	public static volatile SingularAttribute<User, Boolean> deleted;
 	public static volatile ListAttribute<User, Role> belongsTo;
 	public static volatile SingularAttribute<User, String> password;
-	public static volatile SingularAttribute<User, String> firstname;
-	public static volatile SingularAttribute<User, String> surname;
 }
--- scoutmaster/src/main/java/au/org/scoutmaster/domain/accounting/CreditNote_.java
+++ scoutmaster/src/main/java/au/org/scoutmaster/domain/accounting/CreditNote_.java
@@ -6,10 +6,10 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2013-10-22T20:27:35.427+1100")
+@Generated(value="Dali", date="2013-10-23T14:21:31.718+1100")
 @StaticMetamodel(CreditNote.class)
 public class CreditNote_ extends BaseEntity_ {
+	public static volatile SingularAttribute<CreditNote, Long> creditNoteNumber;
 	public static volatile ListAttribute<CreditNote, CreditNoteLine> creditNoteLines;
 	public static volatile SingularAttribute<CreditNote, Invoice> associatedInvoice;
-	public static volatile SingularAttribute<CreditNote, Long> creditNoteNumber;
 }
