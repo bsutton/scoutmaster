64fa3a39670d7b8d5c335cba29dc404fe31fcb6a
bsutton@noojee.com.au <bsutton@noojee.com.au>
2013-10-28 09:31:07 +1100

    working through adding the ability to track purchases.

--- scoutmaster/src/main/java/au/org/scoutmaster/domain/accounting/Purchase.java
+++ scoutmaster/src/main/java/au/org/scoutmaster/domain/accounting/Purchase.java
@@ -11,24 +11,52 @@ import javax.persistence.Table;
 
 import au.org.scoutmaster.domain.BaseEntity;
 import au.org.scoutmaster.domain.Contact;
+import au.org.scoutmaster.domain.Organisation;
 
 @Entity
 @Table(name="Purchase")
 public class Purchase  extends BaseEntity
 {
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * How the purchase was made. Either direct purchase or an expense claim.
+	 * 
+	 * For a direct purchase
+	 */
+	PurchaseMethod method;
 
 	/**
-	 * The contact this purchase was made from.
+	 * The organisation/supplier this purchase was made from.
+	 * 
+	 * This will generally 
 	 */
 	@ManyToOne
-	Contact purchasedFrom;
+	Organisation purchasedFrom;
 	
 	/**
 	 * The group member that made the purchase.
+	 * 
+	 * If the method is EXPENSE_CLAIM then this is the person the group needs to pay.
 	 */
 	@ManyToOne
 	Contact purchasedBy;
+	
+	
+	/**
+	 * The group member that approved the purchase.
+	 */
+	Contact approvedBy;
+	
+	/**
+	 * The reason the goods or services where purchases
+	 */
+	String reason;
+	
+	/**
+	 * A high level description of what was purchased.
+	 */
+	String description;
 	
 	@OneToMany(cascade = CascadeType.ALL)
 	List<PurchaseLine> purchaseLines = new ArrayList<>();
@@ -36,7 +64,7 @@ public class Purchase  extends BaseEntity
 	@Override
 	public String getName()
 	{
-		return purchasedFrom.getFullname() ;
+		return purchasedFrom.getName();
 	}
 	
 }
--- scoutmaster/src/main/java/au/org/scoutmaster/domain/accounting/Purchase_.java
+++ scoutmaster/src/main/java/au/org/scoutmaster/domain/accounting/Purchase_.java
@@ -2,15 +2,20 @@ package au.org.scoutmaster.domain.accounting;
 
 import au.org.scoutmaster.domain.BaseEntity_;
 import au.org.scoutmaster.domain.Contact;
+import au.org.scoutmaster.domain.Organisation;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2013-10-21T18:10:33.179+1100")
+@Generated(value="Dali", date="2013-10-27T08:40:12.598+1100")
 @StaticMetamodel(Purchase.class)
 public class Purchase_ extends BaseEntity_ {
-	public static volatile SingularAttribute<Purchase, Contact> purchasedFrom;
+	public static volatile SingularAttribute<Purchase, Organisation> purchasedFrom;
 	public static volatile SingularAttribute<Purchase, Contact> purchasedBy;
 	public static volatile ListAttribute<Purchase, PurchaseLine> purchaseLines;
+	public static volatile SingularAttribute<Purchase, PurchaseMethod> method;
+	public static volatile SingularAttribute<Purchase, Contact> approvedBy;
+	public static volatile SingularAttribute<Purchase, String> reason;
+	public static volatile SingularAttribute<Purchase, String> description;
 }
