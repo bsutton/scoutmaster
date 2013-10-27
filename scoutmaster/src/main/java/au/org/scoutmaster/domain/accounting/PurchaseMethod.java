30de376bba44be9f2f2fe1c7418e6df8b7e51d06
bsutton@noojee.com.au <bsutton@noojee.com.au>
2013-10-28 09:32:14 +1100

    Added ability to define the method of purchase.

---
+++ scoutmaster/src/main/java/au/org/scoutmaster/domain/accounting/PurchaseMethod.java
@@ -0,0 +1,15 @@
+package au.org.scoutmaster.domain.accounting;
+
+public enum PurchaseMethod
+{
+	/**
+	 * Used when the group directly purchases goods from a supplier.
+	 */
+	DIRECT_PURCHASE
+	
+	/**
+	 * Used when a Group Member purchases goods and then submits an expense claim.
+	 */
+	, EXPENSE_CLAIM;
+
+}
