7c01b094fb307766f3f956b0ce90670a71929ffc
bsutton@noojee.com.au <bsutton@noojee.com.au>
2013-11-13 18:45:39 +1100

    began implementing events.

---
+++ scoutmaster/src/main/java/au/org/scoutmaster/dao/DocumentDao.java
@@ -0,0 +1,34 @@
+package au.org.scoutmaster.dao;
+
+import javax.persistence.EntityManager;
+
+import au.com.vaadinutils.dao.JpaBaseDao;
+import au.org.scoutmaster.domain.Document;
+import au.org.scoutmaster.domain.Document_;
+import au.org.scoutmaster.domain.access.User_;
+
+import com.vaadin.addon.jpacontainer.JPAContainer;
+
+public class DocumentDao extends JpaBaseDao<Document, Long> implements Dao<Document, Long>
+{
+
+	public DocumentDao()
+	{
+		// inherit the default per request em.
+	}
+
+	public DocumentDao(EntityManager em)
+	{
+		super(em);
+	}
+
+
+	@Override
+	public JPAContainer<Document> createVaadinContainer()
+	{
+		JPAContainer<Document> container = super.createVaadinContainer();
+		container.addNestedContainerProperty(new Path(Document_.addedBy, User_.username).getName());
+
+		return container;
+	}
+}
--- scoutmaster/src/main/java/au/org/scoutmaster/dao/EventDao.java
+++ scoutmaster/src/main/java/au/org/scoutmaster/dao/EventDao.java
@@ -1,9 +1,15 @@
 package au.org.scoutmaster.dao;
 
+import java.util.Date;
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import au.com.vaadinutils.dao.JpaBaseDao;
+import au.org.scoutmaster.domain.Contact;
 import au.org.scoutmaster.domain.Event;
+import au.org.scoutmaster.domain.Event_;
 
 import com.vaadin.addon.jpacontainer.JPAContainer;
 
@@ -12,17 +18,55 @@ public class EventDao extends JpaBaseDao<Event, Long> implements Dao<Event, Long
 
 	public EventDao()
 	{
-		// inherit the default per request em. 
+		// inherit the default per request em.
 	}
-	
+
 	public EventDao(EntityManager em)
 	{
 		super(em);
 	}
-	
+
 	@Override
 	public JPAContainer<Event> createVaadinContainer()
 	{
-		return super.createVaadinContainer();
+		JPAContainer<Event> container = super.createVaadinContainer();
+
+		container.sort(new Object[]
+		{ Event_.eventStartDateTime.getName() }, new boolean[]
+		{ true });
+
+		container.addNestedContainerProperty("location.street");
+		container.addNestedContainerProperty("location.city");
+		container.addNestedContainerProperty("location.postcode");
+		container.addNestedContainerProperty("location.state");
+
+		return container;
+	}
+
+	public List<Event> findBetween(Date startDate, Date endDate)
+	{
+		Query query = entityManager.createNamedQuery(Event.FIND_BETWEEN);
+		query.setParameter("startDate", startDate);
+		query.setParameter("endDate", endDate);
+		@SuppressWarnings("unchecked")
+		List<Event> results = query.getResultList();
+		return results;
+
+	}
+
+	public boolean hasCoordinator(au.org.scoutmaster.domain.Event event, Contact contact)
+	{
+		return event.getCoordinators().contains(contact);
+	}
+
+	public void attachCoordinator(au.org.scoutmaster.domain.Event event, Contact contact)
+	{
+		event.getCoordinators().add(contact);
+	}
+
+	public void detachCoordinator(au.org.scoutmaster.domain.Event event, Contact contact)
+	{
+		event.getCoordinators().remove(contact);
+
 	}
 }
