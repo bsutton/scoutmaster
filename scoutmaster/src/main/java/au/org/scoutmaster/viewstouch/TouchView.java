1692c562b74c5332e30fd0d1b3b0652857e20deb
bsutton@noojee.com.au <bsutton@noojee.com.au>
2013-11-13 18:37:28 +1100

    started playing with the touch interface.

---
+++ scoutmaster/src/main/java/au/org/scoutmaster/viewstouch/TouchView.java
@@ -0,0 +1,28 @@
+package au.org.scoutmaster.viewstouch;
+
+import com.vaadin.ui.Button;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.TextField;
+
+// Define a view
+public class TouchView //extends NavigationView
+{
+	private static final long serialVersionUID = 1L;
+//
+//	public TouchView()
+//	{
+//		super("Planet Details");
+//
+//		CssLayout content = new CssLayout();
+//		setContent(content);
+//
+//		VerticalComponentGroup group = new VerticalComponentGroup();
+//		content.addComponent(group);
+//
+//		group.addComponent(new TextField("Planet"));
+//		group.addComponent(new NumberField("Found"));
+//		group.addComponent(new Switch("Probed"));
+//
+//		setRightComponent(new Button("OK"));
+//	}
+}
