6b08d188752ba010c2ed8db294ee18091418311d
bsutton@noojee.com.au <bsutton@noojee.com.au>
2013-11-13 18:44:58 +1100

    began implementing touch kit.

---
+++ scoutmaster/src/main/java/au/org/scoutmaster/application/TouchNavigatorUI.java
@@ -0,0 +1,25 @@
+package au.org.scoutmaster.application;
+
+import au.org.scoutmaster.viewstouch.TouchView;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
+import com.vaadin.annotations.Widgetset;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.UI;
+
+@Theme("mobiletheme")
+@Widgetset("au.org.scoutmaster.AppWidgetSet")
+@Title("Scoutmaster Mobile")
+public class TouchNavigatorUI extends UI
+{
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void init(VaadinRequest request)
+	{
+
+		// Use it as the content root
+	//	setContent(new TouchView());
+	}
+}
