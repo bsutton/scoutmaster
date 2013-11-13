f1951d2f5384af4210d1a24acf372ceb1d857bf4
bsutton@noojee.com.au <bsutton@noojee.com.au>
2013-11-13 18:43:16 +1100

    begin implementing section meetings.

--- scoutmaster/src/main/java/au/org/scoutmaster/domain/SectionMeeting_.java
+++ scoutmaster/src/main/java/au/org/scoutmaster/domain/SectionMeeting_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2013-08-27T23:08:58.284+1000")
+@Generated(value="Dali", date="2013-11-04T12:44:47.494+1100")
 @StaticMetamodel(SectionMeeting.class)
 public class SectionMeeting_ extends Event_ {
 	public static volatile SingularAttribute<SectionMeeting, SectionType> section;
