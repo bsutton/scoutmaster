e557bd67cc254f542de385fc3e7c0824ee3ad237
bsutton@noojee.com.au <bsutton@noojee.com.au>
2013-12-02 09:32:12 +1100

    added a logout option.

---
+++ scoutmaster/src/main/java/au/org/scoutmaster/views/LogoutView.java
@@ -0,0 +1,57 @@
+package au.org.scoutmaster.views;
+
+import au.com.vaadinutils.menu.Menu;
+import au.org.scoutmaster.application.SMSession;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.shared.ui.MarginInfo;
+import com.vaadin.shared.ui.label.ContentMode;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.Reindeer;
+
+@Menu(display = "Logout")
+public class LogoutView extends CustomComponent implements View
+{
+	private static final long serialVersionUID = 1L;
+
+	public static final String NAME = "logout";
+
+	public LogoutView()
+	{
+		setSizeFull();
+
+		VerticalLayout fields = new VerticalLayout();
+		fields.setSpacing(true);
+		fields.setMargin(new MarginInfo(true, true, true, true));
+		fields.setSizeUndefined();
+
+		// Add both to a panel
+		Label label = new Label("<H1>Thankyou for using Scoutmaster.</H1>");
+		label.setContentMode(ContentMode.HTML);
+
+		fields.addComponent(label);
+
+		label = new Label("Click <a href='#Login'>here to login again.</a>");
+		label.setContentMode(ContentMode.HTML);
+		fields.addComponent(label);
+
+		// The view root layout
+		VerticalLayout viewLayout = new VerticalLayout(fields);
+		viewLayout.setSizeFull();
+		viewLayout.setComponentAlignment(fields, Alignment.MIDDLE_CENTER);
+		viewLayout.setStyleName(Reindeer.LAYOUT_BLUE);
+		setCompositionRoot(viewLayout);
+	}
+
+	@Override
+	public void enter(ViewChangeEvent event)
+	{
+		SMSession.INSTANCE.setLoggedInUser(null);
+		getUI().getSession().close();
+		getUI().getPage().setLocation( "/scoutmaster/" );
+	}
+}
