c057d89c125bbd5ee2538f56c9e1c301d11a5c59
bsutton@noojee.com.au <bsutton@noojee.com.au>
2014-02-15 10:31:43 +1100

    new class.

---
+++ scoutmaster/src/main/java/au/org/scoutmaster/dao/QualificationTypeDao.java
@@ -0,0 +1,18 @@
+package au.org.scoutmaster.dao;
+
+import au.com.vaadinutils.dao.JpaBaseDao;
+import au.org.scoutmaster.domain.QualificationType;
+
+import com.vaadin.addon.jpacontainer.JPAContainer;
+
+public class QualificationTypeDao  extends JpaBaseDao<QualificationType, Long>
+{
+
+	@Override
+	public JPAContainer<QualificationType> createVaadinContainer()
+	{
+		return super.createVaadinContainer();
+	}
+	
+}
+
