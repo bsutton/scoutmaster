03f17aacabbdaed0a182d6fefbdac7e554c453dd
bsutton@noojee.com.au <bsutton@noojee.com.au>
2014-02-15 10:31:23 +1100

    moved this to the correct package.

--- scoutmaster/src/main/java/au/com/vaadinutils/domain/Color_.java
+++ scoutmaster/src/main/java/au/com/vaadinutils/domain/Color_.java
@@ -1,10 +1,10 @@
-package au.com.vaadinutils.fields;
+package au.com.vaadinutils.domain;
 
 import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2014-01-31T18:51:24.406+1100")
+@Generated(value="Dali", date="2014-02-12T19:14:46.793+1100")
 @StaticMetamodel(Color.class)
 public class Color_ {
 	public static volatile SingularAttribute<Color, Integer> red;
