1d75c5cc0fc8dc2fcc06b68ac745d50682060aca
bsutton@noojee.com.au <bsutton@noojee.com.au>
2014-03-16 13:30:39 +1100

    fixed the session logger as it doesn't have a em when it fires.

--- scoutmaster/src/main/java/au/org/scoutmaster/application/SessionListener.java
+++ scoutmaster/src/main/java/au/org/scoutmaster/application/SessionListener.java
@@ -2,6 +2,7 @@ package au.org.scoutmaster.application;
 
 import java.util.Date;
 
+import javax.persistence.EntityManager;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
@@ -9,45 +10,56 @@ import javax.servlet.http.HttpSessionListener;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import au.com.vaadinutils.dao.EntityManagerProvider;
 import au.com.vaadinutils.dao.JpaBaseDao;
 import au.org.scoutmaster.dao.DaoFactory;
+import au.org.scoutmaster.dao.Transaction;
 import au.org.scoutmaster.domain.access.SessionHistory;
 import au.org.scoutmaster.domain.access.User;
 
-
-public class SessionListener implements HttpSessionListener {
+public class SessionListener implements HttpSessionListener
+{
 
 	private static Logger logger = LogManager.getLogger();
 
-    @Override
-    public void sessionCreated(HttpSessionEvent arg0) {
-    	logger.debug("Session created");
-    }
+	@Override
+	public void sessionCreated(HttpSessionEvent arg0)
+	{
+		logger.debug("Session created");
+	}
 
-    @Override
-    public void sessionDestroyed(HttpSessionEvent arg0) {
-    	logger.debug("Session destroyed");
-    	
-    	
-    	/**
-    	 * NOTE: this won't work until we move to tomcat 8 as vaadin
-    	 * uses a FakeHttpSession which is something to do with atmosphere.
-    	 * Tomcat 8 resolves this issue.
-    	 * 
-    	 * In the mean time we have no session history.
-    	 */
-    	/**
-    	 * Create a record of the users session.
-    	 */
-		JpaBaseDao<SessionHistory, Long> daoSession = DaoFactory.getGenericDao(SessionHistory.class);
-    	HttpSession session = arg0.getSession();
-    	User user = (User) session.getAttribute(SMSession.USER);
-    	
-    	SessionHistory sessionHistory = new SessionHistory();
-    	sessionHistory.setUser(user);
-    	sessionHistory.setEnd(new Date());
-		sessionHistory.setStart(new Date(session.getCreationTime()));
-    	daoSession.persist(sessionHistory);
-    }
+	@Override
+	public void sessionDestroyed(HttpSessionEvent arg0)
+	{
+		logger.debug("Session destroyed");
+
+		/**
+		 * NOTE: this won't work until we move to tomcat 8 as vaadin uses a
+		 * FakeHttpSession which is something to do with atmosphere. Tomcat 8
+		 * resolves this issue.
+		 * 
+		 * In the mean time we have no session history.
+		 */
+		/**
+		 * Create a record of the users session.
+		 */
+		EntityManager em = EntityManagerProvider.createEntityManager();
+		try (Transaction t = new Transaction(em))
+		{
+			JpaBaseDao<SessionHistory, Long> daoSession = new DaoFactory(em).getSessionHistoryDao();
+			HttpSession session = arg0.getSession();
+			User user = (User) session.getAttribute(SMSession.USER);
+
+			SessionHistory sessionHistory = new SessionHistory();
+			sessionHistory.setUser(user);
+			sessionHistory.setEnd(new Date());
+			sessionHistory.setStart(new Date(session.getCreationTime()));
+			daoSession.persist(sessionHistory);
+			t.commit();
+		}
+		finally
+		{
+		}
+	}
 
 }
