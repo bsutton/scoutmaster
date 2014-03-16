55c8e62e2077daed67760a4c19108152a99c19fd
bsutton@noojee.com.au <bsutton@noojee.com.au>
2014-03-16 13:31:58 +1100

    added new properties. Got the sort working.

--- scoutmaster/src/main/java/au/org/scoutmaster/dao/RaffleAllocationDao.java
+++ scoutmaster/src/main/java/au/org/scoutmaster/dao/RaffleAllocationDao.java
@@ -3,7 +3,10 @@ package au.org.scoutmaster.dao;
 import javax.persistence.EntityManager;
 
 import au.com.vaadinutils.dao.JpaBaseDao;
+import au.com.vaadinutils.dao.Path;
+import au.org.scoutmaster.domain.Contact_;
 import au.org.scoutmaster.domain.RaffleAllocation;
+import au.org.scoutmaster.domain.RaffleAllocation_;
 
 import com.vaadin.addon.jpacontainer.JPAContainer;
 
@@ -24,12 +27,17 @@ public class RaffleAllocationDao extends JpaBaseDao<RaffleAllocation, Long> impl
 	public JPAContainer<RaffleAllocation> createVaadinContainer()
 	{
 		JPAContainer<RaffleAllocation> container = super.createVaadinContainer();
+		
+		container.addNestedContainerProperty(new Path(RaffleAllocation_.allocatedTo, Contact_.fullname).getName());
+		container.addNestedContainerProperty(new Path(RaffleAllocation_.issuedBy, Contact_.fullname).getName());
+		
 
-//		container.sort(new Object[]
-//		{ RaffleAllocation_.dateAllocated.getName(), new Path(RaffleAllocation_.allocatedTo, Contact_.fullname).getName()}, new boolean[]
-//		{ true, true });
+		container.sort(new Object[]
+		{ RaffleAllocation_.dateAllocated.getName(), new Path(RaffleAllocation_.allocatedTo, Contact_.fullname).getName()}, new boolean[]
+		{ true, true });
 		
 
 		return container;
 	}
+
 }
