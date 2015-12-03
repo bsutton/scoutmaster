7cb2f527937d5d8c36e24e57a5285c1a7466fe0b
Brett Sutton <bsutton@noojee.com.au>
2015-12-03 16:05:57 +1100

    Added logic to transpose common mis-spellings of gender.
    Added NonBinary and Other at request of LGBT community.

--- scoutmaster/src/main/java/au/org/scoutmaster/domain/Gender.java
+++ scoutmaster/src/main/java/au/org/scoutmaster/domain/Gender.java
@@ -2,5 +2,43 @@ package au.org.scoutmaster.domain;
 
 public enum Gender
 {
-	Male, Female, Transgender
+	Male, Female, NonBinary, Other;
+
+	public Gender getEnum(String genderString)
+	{
+		Gender gender = Male;
+
+		// Try to translate common gender spellings/mis-spellings
+
+		// a little cleanup.
+		genderString = genderString.toLowerCase().trim();
+
+		// Male translations
+		if (genderString.equals("male")
+				// because i do this all of the time.
+				|| genderString.equals("mail"))
+		{
+			gender = Male;
+		}
+		// female
+
+		else if (genderString.equals("female")
+				// because i do this all of the time.
+
+				|| genderString.equals("femail"))
+		{
+			gender = Female;
+		}
+
+		// transgender
+		else if (genderString.equals("NonBinary") || genderString.equals("Non Binary"))
+		{
+			gender = Female;
+		}
+		else
+			gender = Other;
+
+		return gender;
+
+	}
 }
