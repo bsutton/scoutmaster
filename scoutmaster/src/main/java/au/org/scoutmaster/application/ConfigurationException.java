dd2797fc7ef971d0b7937253c82711ca03850011
Brett Sutton <bsutton@noojee.com.au>
2016-03-07 23:48:30 +1100

    new exception.

---
+++ scoutmaster/src/main/java/au/org/scoutmaster/application/ConfigurationException.java
@@ -0,0 +1,13 @@
+package au.org.scoutmaster.application;
+
+public class ConfigurationException extends Exception
+{
+
+	public ConfigurationException(String message)
+	{
+		super(message);
+	}
+
+	private static final long serialVersionUID = 1L;
+
+}
